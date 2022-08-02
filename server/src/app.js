589169e6e094968d041587fb71938caf06440bc2
MarceloArraes <marcelo.arraes@gmail.com>
2022-08-02 10:49:21 +0000

    feat: serving cliente throught node server using static files

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 const app = express();
@@ -7,7 +8,10 @@ app.use(cors({
   origin: '*',
 }));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, '..','public')));
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;
